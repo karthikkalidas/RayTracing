2ee29847757e2ded840b576974e14ddf408e07f9
karthikkalidas <karthik.kalidas@gmail.com>
2022-12-05 16:51:15 -0700

    master benchmark of 63s

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ src/main.cpp -I include -o main.out
+g++ src/main.cpp -I include -O3 -o main.out
 ./main.out > image.ppm
 xdg-open image.ppm
--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,8 @@ hittable_list random_scene(){
     auto ground_material = std::make_shared<lambertian>(color(0.5, 0.5, 0.5));
     world.items.emplace_back(std::make_shared<sphere>(point3(0, -1000,0), 1000, ground_material));
 
-    for(int a = 0; a < 5; ++a){
-        for(int b = 0; b < 5; ++b){
+    for(int a = -11; a < 11; ++a){
+        for(int b = -11; b < 11; ++b){
             auto choose_mat = random_double();
             point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
 
@@ -75,10 +75,10 @@ int main(){
     auto start = std::chrono::high_resolution_clock::now();
 
     // Image
-    const double aspect_ratio = 3.0 / 2.0;
+    const double aspect_ratio = 16.0 / 9.0;
     const int image_width = 1200;
     const int image_height = (image_width / aspect_ratio);
-    const int samples_per_pixel = 500; 
+    const int samples_per_pixel = 10; 
     const int max_depth = 50;
 
     // World
